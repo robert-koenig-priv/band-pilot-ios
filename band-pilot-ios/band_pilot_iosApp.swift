871d65b5c35842540dc161b157ad3cb000b66782
Rob <robert.koenig2@gmail.com>
2026-07-22 22:13:02 +0200

    Initial Commit

---
+++ band-pilot-ios/band_pilot_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  band_pilot_iosApp.swift
+//  band-pilot-ios
+//
+//  Created by Robert König on 22.07.26.
+//
+
+import SwiftUI
+
+@main
+struct band_pilot_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
